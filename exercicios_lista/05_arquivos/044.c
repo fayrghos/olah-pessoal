d54fa804ea36b585808b35611bdb42caf409f5c6
fayrghos <145410663+fayrghos@users.noreply.github.com>
2025-11-21 20:45:34 -0300

    Correção na tarefa 044

--- exercicios_lista/05_arquivos/044.c
+++ exercicios_lista/05_arquivos/044.c
@@ -8,16 +8,15 @@ typedef struct {
 int main() {
     FILE *txt = fopen("./player_log.txt", "r+");
 
-    int conquista_atual;
-    int experiencia_atual;
     int experiencia_total = 0;
+    Conquista conquista_temp;
 
     int n;
     fscanf(txt, "%i", &n);
 
     for (int i = 0; i < n; i++) {
-        fscanf(txt, "%i %i", &conquista_atual, &experiencia_atual);
-        experiencia_total += experiencia_atual;
+        fscanf(txt, "%i %i", &conquista_temp.id, &conquista_temp.pontos);
+        experiencia_total += conquista_temp.pontos;
     }
 
     fprintf(txt, "\n--- SESSION CONCLUDED ---");
